bc3b05173968078ed9996375b0021b0611ebd847
Emad_Atieh <Emaddeenatieh@gmail.com>
2024-11-14 13:35:02 +0300

    First Commit

--- Mango.Service.CouponAPI/obj/Debug/net8.0/Mango.Service.CouponAPI.AssemblyInfo.cs
+++ Mango.Service.CouponAPI/obj/Debug/net8.0/Mango.Service.CouponAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Service.CouponAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aba44e91bd6fa3ef0aec788ba2137fb3cf827c63")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Service.CouponAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Service.CouponAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
